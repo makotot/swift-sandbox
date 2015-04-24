e1816e54cbc62c9cb578392b3da48b8c3d2442ab
makotot <makoto.ttn@gmail.com>
2015-04-24 18:56:28 +0900

    init

---
+++ swift-sandbox/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  swift-sandbox
+//
+//  Created by tateno on 2015/04/24.
+//  Copyright (c) 2015年 makotot. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ swift-sandbox/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  swift-sandbox
+//
+//  Created by tateno on 2015/04/24.
+//  Copyright (c) 2015 makotot. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+    
+    override func viewDidLoad() {
+        println("hello!")
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override var representedObject: AnyObject? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+    
+}
+
